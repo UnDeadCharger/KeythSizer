2f1bb991e91efb50bfa5e498dc8b0d156b24f488
Truong Gia Huy <huy.truong@devsamurai.com>
2025-10-08 01:11:18 +0700

    KS-2 feat: implementing ctrl-keys for keybinding library

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,53 @@
 import { useEffect, useState } from "react";
 
+import keys, { type Character, type Key } from 'ctrl-keys'
+
 const playNote = (note: HTMLAudioElement) => {
 	note.pause(); // important!!!
 	note.currentTime = 0;
 	note.play();
 };
 
+
 function App() {
 	const [count, setCount] = useState(0);
 
-	const audioElementC = new Audio("src/assets/notes/c4.mp3");
-	const audioElementD = new Audio("src/assets/notes/d4.mp3");
-	const audioElementE = new Audio("src/assets/notes/e4.mp3");
-	const audioElementF = new Audio("src/assets/notes/f4.mp3");
-	const audioElementG = new Audio("src/assets/notes/g4.mp3");
-	const audioElementA = new Audio("src/assets/notes/a5.mp3");
-	const audioElementB = new Audio("src/assets/notes/b5.mp3");
+	const handler = keys();
+
+	const audioElementC4 = new Audio("src/assets/notes/c4.mp3");
+	const audioElementD4= new Audio("src/assets/notes/d4.mp3");
+	const audioElementE4 = new Audio("src/assets/notes/e4.mp3");
+	const audioElementF4 = new Audio("src/assets/notes/f4.mp3");
+	const audioElementG4 = new Audio("src/assets/notes/g4.mp3");
+	const audioElementA5 = new Audio("src/assets/notes/a5.mp3");
+	const audioElementB5 = new Audio("src/assets/notes/b5.mp3");
 	const audioElementC5 = new Audio("src/assets/notes/c5.mp3");
 
-	const keyboardPlaying = (event: KeyboardEvent) => {
-		switch (event.key) {
-			case "a":
-				playNote(audioElementC);
-				break;
-			case "s":
-				playNote(audioElementD);
-				break;
-			case "d":
-				playNote(audioElementE);
-				break;
-			case "f":
-				playNote(audioElementF);
-				break;
-			case "g":
-				playNote(audioElementG);
-				break;
-			case "h":
-				playNote(audioElementA);
-				break;
-			case "j":
-				playNote(audioElementB);
-				break;
-			case "k":
-				playNote(audioElementC5);
-				break;
-			default:
-				break;
-		}
-	};
+	const DEFAULT_KEY_MAPPING: Record<string, HTMLAudioElement> = {
+		"a": audioElementC4,
+		"s": audioElementD4,
+		"d": audioElementE4,
+		"f": audioElementF4,
+		"g": audioElementG4,
+		"h": audioElementA5,
+		"j": audioElementB5,
+		"k": audioElementC5,
+	}
+	
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
+		Object.entries(DEFAULT_KEY_MAPPING).forEach(([key, audioElement]) => {
+			handler.add(key as Key, () => playNote(audioElement));
+		});
 		window.addEventListener("keydown", (event) => {
-			keyboardPlaying(event);
+			// keyboardPlaying(event);
+			handler.handle(event);
 		});
 		return window.removeEventListener("keydown", () => {
 			console.log("unmounted");
 		});
 	}, []);
-
 	return (
 		<>
 			<div>
@@ -77,79 +66,21 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
-
-			<button
-				type="button"
-				onClick={() => {
-					playNote(audioElementC);
-				}}
-			>
-				{" "}
-				C$ note
-			</button>
-			<button
-				type="button"
-				onClick={() => {
-					playNote(audioElementD);
-				}}
-			>
-				{" "}
-				D$ note
-			</button>
-			<button
-				type="button"
-				onClick={() => {
-					playNote(audioElementE);
-				}}
-			>
-				{" "}
-				E$ note
-			</button>
-			<button
-				type="button"
-				onClick={() => {
-					playNote(audioElementF);
-				}}
-			>
-				{" "}
-				F$ note
-			</button>
-			<button
-				type="button"
-				onClick={() => {
-					playNote(audioElementG);
-				}}
-			>
-				{" "}
-				G$ note
-			</button>
-			<button
-				type="button"
-				onClick={() => {
-					playNote(audioElementA);
-				}}
-			>
-				{" "}
-				A$ note
-			</button>
-			<button
-				type="button"
-				onClick={() => {
-					playNote(audioElementB);
-				}}
-			>
-				{" "}
-				B$ note
-			</button>
-			<button
-				type="button"
-				onClick={() => {
-					playNote(audioElementC5);
-				}}
-			>
-				{" "}
-				C$$ note
-			</button>
+			{Object.entries(DEFAULT_KEY_MAPPING).map(([key, audioElement]) => {
+				return (
+					<button
+						key={key}
+						type="button"
+						onClick={() => {
+							playNote(audioElement);
+						}}
+					>				
+{						audioElement.src.slice(-6, -4)
+}						{" "}
+						({key.toUpperCase()}) note
+					</button>
+				);
+		})}
 		</>
 	);
 }
@@ -157,3 +88,9 @@ function App() {
 export default App;
 
 //PROBLEM TO SOLVE
+//Audio get cut off
+//Cannot have multiple notes playing at the same time
+//Holding a key repeat the note repeatedly - instead it should trail off
+//Buttons are not styled - no indicator when it is pressed
+	//indicator when press
+	// Keys mapping feature
